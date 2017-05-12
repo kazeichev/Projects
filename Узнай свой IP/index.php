b15888581515fc4c8cdd8f8e6425234f1912fd7c
Nikita Kazeichev <kazeichev@yandex.ru>
2017-05-13 01:54:02 +0300

    new version

--- Узнай свой IP/index.php
+++ Узнай свой IP/index.php
@@ -8,22 +8,22 @@
 </head>
 <body>
   <script type="text/javascript">
- 		function show(state){
-          document.getElementById('window').style.display = state;
-					document.getElementById('wrap').style.display = state;
-			}
-		</script>
-    <div id="window" style="display: none">
-      <form action="" method="post">
-        <input type="text" name="ip" value="
-        <?php
-        if(isset($_SERVER['REMOTE_ADDR'])) {
-          echo trim($_SERVER['REMOTE_ADDR']);
-        }
-        ?>" id="yourIP">
-      </form>
-    </div>
-    <button type="button" id="btn" name="button" onclick="show('block')">Узнай свой IP!</button>
+  function show(state){
+    document.getElementById('window').style.display = state;
+    document.getElementById('wrap').style.display = state;
+  }
+  </script>
+  <div id="window" style="display: none">
+    <form action="" method="post">
+      <input type="text" name="ip" value="
+      <?php
+      if(isset($_SERVER['REMOTE_ADDR'])) {
+        echo trim($_SERVER['REMOTE_ADDR']);
+      }
+      ?>" id="yourIP">
+    </form>
+  </div>
+  <button type="button" id="btn" name="button" onclick="show('block')">Узнай свой IP!</button>
 
 </body>
 </html>
